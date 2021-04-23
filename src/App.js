baf6e9b16819e2d79fd010cb9ef73535b1249bc2
Adam Mann <aemann2@gmail.com>
2021-04-23 09:44:55 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import useSWR from 'swr';
+// pulling in the pre-configured fetcher (which uses fetch) from utils
+import { fetcher } from './utils/fetcher';
+import './styles.css';
 
-function App() {
+export default function App() {
+  const [pageIndex, setPageIndex] = useState(0);
+
+  // passing the pagination state to the endpoint
+  const url = `https://gp-super-store-api.herokuapp.com/item/list?size=6&sortDir=asc&from=${pageIndex}`;
+
+  // this is the whole fetch operation...very concise
+  const { data, error } = useSWR(url, fetcher);
+  if (error) return 'An error has occurred.';
+  if (!data) return 'Loading...';
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {data.items.map((item) => (
+        <div key={item._id}>{item.name}</div>
+      ))}
+      <button onClick={() => setPageIndex(pageIndex >= 6 && pageIndex - 6)}>
+        Previous
+      </button>
+      <button
+        onClick={() =>
+          setPageIndex(data.hasMore ? pageIndex + 6 : pageIndex + 0)
+        }
+      >
+        Next
+      </button>
     </div>
   );
 }
-
-export default App;
